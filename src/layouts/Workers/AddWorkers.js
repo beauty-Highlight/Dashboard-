a89a8ffe5f22ba5c8eaee87072eb229170f2030b
Nada <moonup123@gmail.com>
2022-11-16 11:31:32 +0300

    ٌReviwe Done Delete and update

---
+++ src/layouts/Workers/AddWorkers.js
@@ -0,0 +1,79 @@
+// import DashboardLayout from "examples/LayoutContainers/DashboardLayout"
+// import DashboardNavbar from "examples/Navbars/DashboardNavbar"
+
+// // @mui material components
+// import dayjs from "dayjs";
+// import Grid from "@mui/material/Grid";
+// import Card from "@mui/material/Card";
+// import MDBox from "components/MDBox";
+// import MDTypography from "components/MDTypography";
+// import { TextField } from "@mui/material";
+
+
+// import Button from "@mui/material/Button";
+// import Icon from "@mui/material/Icon";
+// import { useContext, useRef, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { AuthContext } from "context/Auth";
+
+// function AddWorker() {
+//     const { token } = useContext(AuthContext);
+//     const [admin, setAdmin]= useState({
+//         name:'',
+//         email:'',
+//         password:'',
+//         passwordConfirmation:'',
+ 
+//     })
+//     const navigate = useNavigate()
+//     const addAdmin = async (event) => {
+//         event.preventDefault()
+//         console.log(admin)        
+//         const added = await fetch(`http://localhost:3000/admins`, {
+//             method: 'POST',
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 'Authorization': `Bearer ${token}`,
+//                 'Authorization': `Bearer ${token}`,
+//             },
+//             body:  JSON.stringify(admin)
+//         })
+//         const json = await added.json()
+//         console.log(json)
+//         alert(json.messages.join(' '))
+//         if (json.success) {
+//             navigate('/admins')
+//         }
+//     }
+//     return (
+//         <DashboardLayout>
+//             <DashboardNavbar />
+//             <Grid container spacing={6}>
+//                 <Grid item xs={12}>
+//                     <Card>
+//                         <form method="POST" onSubmit={addAdmin}>
+//                             <MDBox p={3}>
+//                                 <MDTypography variant='h5'>Add New Admin</MDTypography>
+//                                 <MDBox pt={4} pb={2}>
+//                                     <MDBox mb={3}><TextField name="name" fullWidth label="name" value={admin.name} onChange={(e) => setAdmin({...admin,name: e.target.value})}/></MDBox>
+//                                     <MDBox mb={3}><TextField name="email" fullWidth label="Email" value={admin.email} onChange={(e) => setAdmin({...admin, email: e.target.value})} /></MDBox>
+//                                     <MDBox mb={3}><TextField name="password" fullWidth label="Password" value={admin.password} onChange={(e) => setAdmin({...admin, password: e.target.value})} /></MDBox>
+//                                     <MDBox mb={3}><TextField name="password" fullWidth label="PasswordConfirmation" value={admin.passwordConfirmation} onChange={(e) => setAdmin({...admin, passwordConfirmation: e.target.value})} /></MDBox>
+//                                     <MDBox>
+//                                         <Button variant="contained" type="submit">
+//                                             <MDTypography color='white' variant="p">
+//                                                 Add A New Admin
+//                                             </MDTypography>
+//                                         </Button>
+//                                     </MDBox>
+//                                 </MDBox>
+//                             </MDBox>
+//                         </form>
+//                     </Card>
+//                 </Grid>
+//             </Grid>
+//         </DashboardLayout>
+//     )
+// }
+
+// export default AddWorker
--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,3 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-Coded by www.creative-tim.com
- =========================================================
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-/** 
-  All of the routes for the Material Dashboard 2 React are added here,
-  You can add a new route, customize the routes and delete the routes here.
-  Once you add a new route on this file it will be visible automatically on
-  the Sidenav.
-  For adding a new route you can follow the existing routes in the routes array.
-  1. The `type` key with the `collapse` value is used for a route.
-  2. The `type` key with the `title` value is used for a title inside the Sidenav. 
-  3. The `type` key with the `divider` value is used for a divider between Sidenav items.
-  4. The `name` key is used for the name of the route on the Sidenav.
-  5. The `key` key is used for the key of the route (It will help you with the key prop inside a loop).
-  6. The `icon` key is used for the icon of the route on the Sidenav, you have to add a node.
-  7. The `collapse` key is used for making a collapsible item on the Sidenav that has other routes
-  inside (nested routes), you need to pass the nested routes inside an array as a value for the `collapse` key.
-  8. The `route` key is used to store the route location which is used for the react router.
-  9. The `href` key is used to store the external links location.
-  10. The `title` key is only for the item with the type of `title` and its used for the title text on the Sidenav.
-  10. The `component` key is used to store the component of its route.
-*/
 
 // Material Dashboard 2 React layouts
 import Dashboard from "layouts/dashboard";
@@ -40,24 +10,17 @@ import Authors from "layouts/authors"
 import Profile from "layouts/profile";
 import Customer from "layouts/Customers";
 import SignIn from "layouts/authentication/sign-in";
-import SignUp from "layouts/authentication/sign-up";
-import Users from "layouts/users";
-import Books from "layouts/Books";
-import AddBook from "layouts/Books/AddBook";
+// import SignUp from "layouts/authentication/sign-up";
 import Admins from "layouts/Admins";
-import AddUser from "layouts/users/AddUser";
 import Review from "layouts/Reviews";
-import Comment from "layouts/Comments";
-import Category from "layouts/Categories";
-import AddCategory from "layouts/Categories/AddCategory";
 // import Reviews from "layouts/review";
 //react icons
 import { RiQuillPenLine } from "react-icons/ri";
 import { MdPeopleAlt } from "react-icons/md";
 import { RiAdminLine } from "react-icons/ri";
-import { BiCategoryAlt } from "react-icons/bi";
-import { SiBookstack } from "react-icons/si";
-import { BiCommentDetail } from "react-icons/bi";
+// import { BiCategoryAlt } from "react-icons/bi";
+// import { SiBookstack } from "react-icons/si";
+// import { BiCommentDetail } from "react-icons/bi";
 import { MdRateReview } from "react-icons/md";
 
 
@@ -66,9 +29,9 @@ import { MdRateReview } from "react-icons/md";
 // @mui icons
 import Icon from "@mui/material/Icon";
 // import AddTrip from "layouts/trips/AddTrip";
-import EditUser from "layouts/users/EditUser";
-import EditBook from "layouts/Books/EditBook";
-import EditCategory from "layouts/Categories/EditCategory";
+// import EditUser from "layouts/users/EditUser";
+// import EditBook from "layouts/Books/EditBook";
+// import EditCategory from "layouts/Categories/EditCategory";
 import EditAdmin from "layouts/Admins/EditAdmin";
 import EditCustomers from "layouts/Customers/EditCustomers";
 import AddAdmin from "layouts/Admins/AddAdmin";
@@ -119,6 +82,33 @@ const routes = [
     component: <AddAdmin />,
     sidenav: false,
   },
+  // {
+  //   type: "collapse",
+  //   name: "Workers",
+  //   key: "workers",
+  //   icon: <RiAdminLine />,
+  //   route: "/workers",
+  //   component: <Workers />,
+  //   sidenav: true,
+  // },
+  // {
+  //   type: "collapse",
+  //   name: "Workers",
+  //   key: "workers",
+  //   icon: <RiAdminLine />,
+  //   route: "/workers/:id",
+  //   component: <EditWorker />,
+  //   sidenav: false,
+  // },
+  // {
+  //   type: "collapse",
+  //   name: "Add Worker",
+  //   key: "add-worker",
+  //   icon: <Icon fontSize="small">forest</Icon>,
+  //   route: "/workers/add",
+  //   component: <AddWorker />,
+  //   sidenav: false,
+  // },
   {
     type: "collapse",
     name: "Customers",
@@ -230,62 +220,6 @@ const routes = [
     component: <Authors />,
     sidenav: false,
   },
- 
-  {
-    type: "collapse",
-    name: "Categories",
-    key: "categories",
-    icon: <BiCategoryAlt />,
-    route: "/categories",
-    component: <Category />,
-    sidenav: true,
-  },
-  {
-    type: "collapse",
-    name: "Add Category",
-    key: "add-Category",
-    icon: <Icon fontSize="small">forest</Icon>,
-    route: "/categories/add",
-    component: <AddCategory />,
-    sidenav: false,
-  },
-  {
-    type: "collapse",
-    name: "Edit Category",
-    key: "edit-Category",
-    icon: <Icon fontSize="small">forest</Icon>,
-    route: "/categories/:id",
-    component: <EditCategory />,
-    sidenav: false,
-  },
-  {
-    type: "collapse",
-    name: "Books",
-    key: "books",
-    icon: <SiBookstack />,
-    route: "/books",
-    component: <Books />,
-    sidenav: true,
-  },
-  {
-    type: "collapse",
-    name: "Add Book",
-    key: "add-Book",
-    icon: <Icon fontSize="small">forest</Icon>,
-    route: "/books/add",
-    component: <AddBook />,
-    sidenav: false,
-  },
- 
-  {
-    type: "collapse",
-    name: "Edit book",
-    key: "Edit-book",
-    icon: <Icon fontSize="small">forest</Icon>,
-    route: "/books/edit/:id",
-    component: <EditBook />,
-    sidenav: false,
-  },
   {
     type: "collapse",
     name: "Reviews",
@@ -312,51 +246,6 @@ const routes = [
     route: "/reviews/:id",
     component: <EditReview />,
     sidenav: false,
-  },
-  // {
-  //   type: "collapse",
-  //   name: "Tables",
-  //   key: "tables",
-  //   icon: <Icon fontSize="small">table_view</Icon>,
-  //   route: "/tables",
-  //   component: <Tables />,
-  //   sidenav: true,
-  // },
-  // {
-  //   type: "collapse",
-  //   name: "Billing",
-  //   key: "billing",
-  //   icon: <Icon fontSize="small">receipt_long</Icon>,
-  //   route: "/billing",
-  //   component: <Billing />,
-  //   sidenav: true,
-  // },
-  // {
-  //   type: "collapse",
-  //   name: "RTL",
-  //   key: "rtl",
-  //   icon: <Icon fontSize="small">format_textdirection_r_to_l</Icon>,
-  //   route: "/rtl",
-  //   component: <RTL />,
-  //   sidenav: true,
-  // },
-  // {
-  //   type: "collapse",
-  //   name: "Notifications",
-  //   key: "notifications",
-  //   icon: <Icon fontSize="small">notifications</Icon>,
-  //   route: "/notifications",
-  //   component: <Notifications />,
-  //   sidenav: true,
-  // },
-  {
-    type: "collapse",
-    name: "Comments",
-    key: "Comments",
-    icon: <BiCommentDetail />,
-    route: "/comment",
-    component: <Comment />,
-    sidenav: true,
   },
 
   {
